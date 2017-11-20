90f10f32e98b3f2103a6db4a0871002ee9d22245
wwwuxt <aiwuxt@gmail.com>
2017-11-20 21:18:05 +0800

    restful-api v1.0

--- python-restful-api/app.py
+++ python-restful-api/app.py
@@ -1,14 +1,27 @@
-#!flask/bin/python
-from flask import Flask
-
+#!venv/bin/python
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python turorial on the web',
+        'done': False
+    }
+]
 
-@app.route('/')
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def index():
-    return "Hello world"
+    return jsonify({'tasks': tasks})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
