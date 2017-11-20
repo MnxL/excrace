c4670bc9bf567d6ca5e87bfb1be8be2356afcc67
wwwuxt <aiwuxt@gmail.com>
2017-11-20 22:38:18 +0800

    restful-api v1.0 task-id

--- python-restful-api/app.py
+++ python-restful-api/app.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
-from flask import Flask,jsonify
+from flask import Flask,jsonify,abort,make_response
 
 app = Flask(__name__)
 
@@ -18,10 +18,21 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/')
 def index():
-    return jsonify({'tasks': tasks})
+    return 'Hello World'
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    print(task)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
