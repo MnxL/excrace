ad7c45d8e7408499bde517aebcac57efc02bd1ce
wwwuxt <aiwuxt@gmail.com>
2018-01-12 14:13:57 +0800

    运算符

--- shell-learn-Bash/opt.sh
+++ shell-learn-Bash/opt.sh
@@ -36,3 +36,46 @@ fi
 # 逻辑运算符
 # && [[ $a -lt 100 && $b -gt 100 ]]
 # ||
+
+# 字符串运算符
+# = 字符串是否相等
+# != 
+# -z 字符串长度是否为0
+# -n 是否为0
+# str 字符串是否为空
+
+stra="abc"
+strb="efg"
+
+if [ $stra = $strb ]
+then
+    echo "$stra = $strb : stra 等于 strb"
+else
+    echo "$stra = $strb : stra 不等于 strb"
+fi
+if [ -z $stra ]
+then 
+    echo "-z $stra : stra 长度为0"
+else
+    echo "-z $stra : stra 长度不为0"
+fi
+if [ $stra ]
+then
+    echo "$stra : 字符串不为空"
+else
+    echo "$stra : 字符串为空"
+fi
+
+
+# 文件测试运算符
+# -b file 快设备文件
+# -c file 字符设备文件
+# -d file 目录
+# -f file 普通文件（既不是目录，也不是设备文件）
+# -g file 是否设置了SGID位
+# -k file 是否设置了粘着位（Sticky Bit）
+# -p file 是否是有名管道
+# -u file 是否设置了SUID位
+# -r/w/x file 是否可读可写可执行
+# -s file 文件是否为空（文件大小是否大于0）
+# -e file 文件（目录）是否存在
